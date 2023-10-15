3fd9c568630118a2d20832583a9ca09348a9448a
choidongkuen <danaver12@daum.net>
2023-10-15 23:48:59 +0900

    feat : JwtAuthenticationFilter 구현

---
+++ src/main/java/com/example/pre_onboarding/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.example.pre_onboarding.filter;
+
+import com.example.pre_onboarding.config.JwtProperties;
+import com.example.pre_onboarding.domain.User;
+import com.example.pre_onboarding.dto.user.UserAuthentication;
+import com.example.pre_onboarding.exception.JwtAuthenticationException;
+import com.example.pre_onboarding.repository.UserRepository;
+import com.example.pre_onboarding.service.JwtService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * 인증이 필요한 회원 API 요청 시, jwt 인증 용도의 필터
+ * - 인증 마다 SecurityContext 생성 후 저장
+ **/
+@Getter
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final JwtProperties jwtProperties;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equals("OPTIONS")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            log.info("checkAccessTokenAndAuthentication() called!");
+            String accessToken = this.checkAccessTokenAndAuthentication(request);
+
+            if(accessToken == null) {
+                throw new JwtAuthenticationException("jwt Authentication exception occurs!");
+            }
+
+            // 3. Access Token 을 파싱해서 User 정보 가져오기
+            User user = this.userRepository.findByUserId(this.jwtService.getUserIdFromToken(accessToken))
+                    .orElseThrow(() -> new UsernameNotFoundException("일치하는 회원이 존재하지 않습니다."));
+
+            // 4. Thread Local 로 동작 하는 SecurityContext 에 저장
+            UserAuthentication userAuthentication = new UserAuthentication(user);
+            SecurityContextHolder.getContext().setAuthentication(userAuthentication);
+        } catch (JwtAuthenticationException | UsernameNotFoundException exception) {
+            log.error("JwtAuthentication Authentication Exception Occurs! - {}",exception);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    // 요청 Authorization 헤더 에서 jwt 유효성 검증 후 리턴
+    private String checkAccessTokenAndAuthentication(HttpServletRequest request) {
+
+        // 1. HttpServletRequest 에서 Access Token 파싱
+        Optional<String> token = this.jwtService.extractAccessToken(request);
+        if (token.isEmpty() || !this.jwtService.validateToken(token.get())) {
+            return null;
+        }
+        return token.get();
+    }
+}
